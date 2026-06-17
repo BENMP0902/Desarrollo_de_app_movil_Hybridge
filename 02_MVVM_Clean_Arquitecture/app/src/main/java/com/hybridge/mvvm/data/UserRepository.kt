1d2d06a01b09a34a424b0ff8359b6925c280f54e
BENMP0902 <benjamin2401128@hybridge.education>
2026-06-17 01:27:16 -0600

    feat(domain): agrega GetEdadUseCase y getUserAge en Repository

--- 02_MVVM_Clean_Arquitecture/app/src/main/java/com/hybridge/mvvm/data/UserRepository.kt
+++ 02_MVVM_Clean_Arquitecture/app/src/main/java/com/hybridge/mvvm/data/UserRepository.kt
@@ -2,4 +2,5 @@ package com.hybridge.mvvm.data
 
 class UserRepository {
     fun getUserName(): String = "David"
+    fun getUserAge(): Int = 25 // Nuevo método que devuelve la edad del usuuario
 }
--- 02_MVVM_Clean_Arquitecture/app/src/main/java/com/hybridge/mvvm/domain/usecase/GetEdadUseCase.kt
+++ 02_MVVM_Clean_Arquitecture/app/src/main/java/com/hybridge/mvvm/domain/usecase/GetEdadUseCase.kt
@@ -1,2 +1,7 @@
 package com.hybridge.mvvm.domain.usecase
 
+import com.hybridge.mvvm.data.UserRepository
+
+class GetEdadUseCase(private val repository: UserRepository) {
+    fun execute(): Int = repository.getUserAge()
+}
