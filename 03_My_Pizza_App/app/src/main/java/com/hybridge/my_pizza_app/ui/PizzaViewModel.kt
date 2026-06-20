ce14650ae7c6495999568ca66d6130074f947714
BENMP0902 <benjamin2401128@hybridge.education>
2026-06-19 23:00:25 -0600

    feat(ui): expose pizzaState with mutableStateOf, add refreshPizza

--- 03_My_Pizza_App/app/src/main/java/com/hybridge/my_pizza_app/ui/PizzaViewModel.kt
+++ 03_My_Pizza_App/app/src/main/java/com/hybridge/my_pizza_app/ui/PizzaViewModel.kt
@@ -1,11 +1,19 @@
 package com.hybridge.my_pizza_app.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.hybridge.my_pizza_app.data.repository.PizzaRepositoryImpl
 import com.hybridge.my_pizza_app.domain.model.Pizza
-import com.hybridge.my_pizza_app.domain.usecases.GetPizzasUseCase
+import com.hybridge.my_pizza_app.domain.usecases.GetPizzaOfDayUseCase
 
 class PizzaViewModel: ViewModel() {
-    private val getPizzasUseCase = GetPizzasUseCase(PizzaRepositoryImpl())
-    fun getPizzaOfTheDay(): Pizza = getPizzasUseCase.execute()
+    private val getPizzasUseCase = GetPizzaOfDayUseCase(PizzaRepositoryImpl())
+    var  pizzaState by mutableStateOf(getPizzasUseCase.execute())
+    fun refreshPizza() {
+        pizzaState = getPizzasUseCase.execute()
+    }
+
 }
+
